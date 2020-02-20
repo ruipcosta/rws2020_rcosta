2a217c7cd6e0446ccfafc5c939158125a5c55287
Rui <ruipcosta@ua.pt>
2020-02-20 10:41:18 +0000

    Initial commit of the ros package
    Passar para pyhton

--- player_rcosta/src/player_node.py
+++ player_rcosta/src/player_node.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+
+def callback(msg):
+
+    print("received a message!")
 
 
 def main():
     print("Hello player node");
+
+    rospy.init_node('rcosta', anonymous=False)
+
+    rospy.Subscriber("chatter",String, callback)
+
+    rospy.spin()
 
 if __name__ == "__main__":
     main()
